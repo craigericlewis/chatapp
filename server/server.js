33bc7a2bc8e13305872acbb07f23451b02ec953c
Craig Lewis <craigericlewis@gmail.com>
2018-07-15 18:24:32 -0400

    Chat Application

---
+++ server/server.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+const {
+  generateMessage,
+  generateLocationMessage
+} = require('./utils/message');
+const {
+  isRealString
+} = require('./utils/validation');
+const {
+  Users
+} = require('./utils/users');
+
+var conString  ="mongodb://craiglewis1508@gmail.com:metroidprime15@ds233581.mlab.com:33581/chatappinfo";
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var users = new Users();
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
+
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+
+  socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
+    callback();
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+  });
+
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
+});
+
+var User = mongoose.model("User", {
+    firstName: String,
+    lastName: String
+})
+
+mongoose.connect(conString, {
+  useMongoClient: true
+}, () => {
+  console.log("DB is connected")
+  saveData()
+});
+
+function saveData() {
+  var user = new User({
+    firstName: "Sibeesh",
+    lastName: "Venu"
+  });
+  user.save();
+}
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
